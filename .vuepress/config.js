f657909adee7edbeedb7139bda140126b3bc0dd4
wemlion <angusfu1126@qq.com>
2019-01-31 16:12:59 +0800

    docs: update config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,7 +3,17 @@ module.exports = {
   title: 'Vuepress Playground Plugin',
   dest: require('path').resolve(__dirname, '../docs'),
   plugins: [require('../lib')],
+  serviceWorker: true,
   themeConfig: {
-    repo: 'AngusFu/vuepress-plugin-playground'
+    repo: 'AngusFu/vuepress-plugin-playground',
+    editLinks: true,
+    editLinkText: '在 GitHub 上编辑此页',
+    lastUpdated: '上次更新',
+    serviceWorker: {
+      updatePopup: {
+        message: '发现新内容可用',
+        buttonText: '刷新'
+      }
+    }
   }
 }
